eb2ea7bc16936dbc98132745223d53ec0d0deacd
yordan17 <iordanvitanov96@gmail.com>
2026-07-05 17:36:44 +0300

    chore: initial Spring Boot project setup

---
+++ src/test/java/app/pawnshop/PawnshopApplicationTests.java
@@ -0,0 +1,13 @@
+package app.pawnshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawnshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
